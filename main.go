8780f506a731602c7b5f5fc13ecf83893e42668f
ZedLin <zed.lin@foxmail.com>
2020-09-13 13:08:35 +0800

    implement handle functions

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"net/http"
+	"strconv"
 )
 
 var db *gorm.DB
@@ -34,4 +36,115 @@ func main() {
 		v1.DELETE("/:id", deleteTodo)
 	}
 	router.Run()
+}
+func createTodo(c *gin.Context) {
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	todo := todoModel{Title: c.PostForm("title"), Completed: completed}
+	db.Save(&todo)
+	c.JSON(http.StatusCreated, gin.H{
+		"status": http.StatusCreated,
+		"message": "Todo item created successfully!",
+		"resourceID": todo.ID,
+	})
+}
+
+func deleteTodo(c *gin.Context) {
+	// 先根据 c.Param("id") 找到 record
+	var todo todoModel
+	todoID := c.Param("id")
+	db.First(&todo, todoID)
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status": http.StatusNotFound,
+			"message": "No todo found!",
+		})
+		return
+	}
+
+	// 再删除
+	db.Delete(&todo)
+}
+
+func fetchSingleTodo(c *gin.Context) {
+	// 根据 c.Param("id") 查询
+	var todo todoModel
+	todoID := c.Param("id")
+	db.First(&todo, todoID)
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status": http.StatusNotFound,
+			"message": "No todo found!",
+		})
+		return
+	}
+
+	// 生成响应的数据并响应
+	completed := false
+	if todo.Completed == 1{
+		completed = true
+	} else {
+		completed = false
+	}
+	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed}
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data": _todo,
+	})
+}
+
+func fetchAllTodo(c *gin.Context) {
+	// 多条记录的 slice
+	var todos []todoModel
+	var _todos []transformedTodo
+	db.Find(&todos)
+	if len(todos) <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status": http.StatusNotFound,
+			"message": "No todo found !",
+		})
+		return
+	}
+
+	// 遍历逐条 record 处理 并响应
+	for _, item := range todos {
+		completed := false
+		if item.Completed == 1 {
+			completed = true
+		} else {
+			completed = false
+		}
+		// transforms the todos for building a good response
+		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, Completed: completed})
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusOK,
+			"data":_todos,
+		})
+	}
+}
+
+
+func updateTodo(c *gin.Context) {
+	// 先根据 id 找到 record
+	var todo todoModel
+	todoID := c.Param("id")
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status": http.StatusNotFound,,
+			"message": "No todo found!",
+		})
+		return
+	}
+
+	// 再逐个字段更新
+	db.Model(&todo).Update("title", c.PostForm("title"))
+
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	db.Model(&todo).Update("completed", completed)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"message": "Todo updated successfully!",
+	})
 }
